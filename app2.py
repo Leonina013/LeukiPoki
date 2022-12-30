f2b208dba873b8f40c4f8c4fac6c2d4abffaeb3e
Leonina013 <118098471+Leonina013@users.noreply.github.com>
2022-12-31 01:23:04 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 from PIL import Image
 import tensorflow as tf
@@ -12,7 +13,7 @@ from keras.models import load_model
 from PIL import Image, ImageOps #Install pillow instead of PIL
 import numpy as np
  
-
+os.environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_devices'
 
 
 
